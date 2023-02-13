dc913f7ce276c1eb29a67a67f1cb2758535383a6
Eglo <UNUstifA4W346TcF2Qwc@gmail.com>
2023-02-13 17:19:46 +0000

    Updated finding

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,43 @@
-#[macro_use] extern crate rocket;
-use std::collections::VecDeque;
-use std::fs::File;
-use std::io::BufWriter;
-use std::io::{BufRead, BufReader, Write};
-
-fn search_one(filename: &str, search_string: &str) -> Result<String, std::io::Error> {
-    let file = File::open(filename)?;
-    let mut reader = BufReader::with_capacity(2048 * 2048, file);
-    let start = std::time::Instant::now();
-    loop {
-        let mut line = String::new();
-        let n = reader.read_line(&mut line)?;
-        if n == 0 {
-            break;
-        }
-        if line.contains(search_string) {
-            println!("Line containing {} found: {}", search_string, line);
-            return Ok(line);
-        }
-    }
-    let elapsed = start.elapsed();
-    println!("Elapsed time: {:?}", elapsed);
-    Ok(String::new())
-}
+#[macro_use]
+extern crate rocket;
+mod find;
 
 #[get("/")]
-fn index() -> String {
-    let result = search_one("Test.json", r#""08934":420696969"#);
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+//"08934":420696969
+
+#[post("/query-one/<table>", data = "<data>")]
+async fn queryOne(table: &str, data: &str) -> String {
+    println!("Table: {}", table);
+    println!("Data: {}", data);
+    let extension: &str = ".json";
+    let together = format!("{}{}", table, extension);
+    let result = find::search_one(&together, data);
     let result_string = match result {
-        Ok(matching_line) => format!("{}", matching_line),
-        Err(error) => format!("An error occurred: {}", error),
+        Ok(matching_line) => format!("{:?}", matching_line),
+        Err(error) => format!(r#"{{"error":"{}"}}"#, error),
     };
     result_string
 }
 
-
-
-#[get("/test")]
-fn test() -> String {
-    format!("Hello, test")
+#[post("/query-many/<table>", data = "<data>")]
+async fn queryMany(table: &str, data: &str) -> String {
+    println!("Table: {}", table);
+    println!("Data: {}", data);
+    let extension: &str = ".json";
+    let together = format!("{}{}", table, extension);
+    let result = find::search_many(&together, data);
+    let result_string = match result {
+        Ok(matching_lines) => format!("{:?}", matching_lines),
+        Err(error) => format!(r#"{{"error":"{}"}}"#, error),
+    };
+    result_string
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index])
-        .mount("/", routes![test])
+    rocket::build().mount("/", routes![index, queryOne, queryMany])
 }
