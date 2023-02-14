e100ed2257a0ad06cc2e10280e744721d21358b9
Eglo Development <UNUstifA4W346TcF2Qwc@gmail.com>
2023-02-14 18:02:45 +0000

    Added JSON file again

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ async fn queryOne(table: &str, data: &str) -> String {
     println!("Table: {}", table);
     println!("Data: {}", data);
     let extension: &str = ".json";
-    let query: &str = functions::query_format(data);
     let together = format!("{}{}", table, extension);
+    let query: &str = functions::query_format(data);
     let result = find::search_one(&together, query);
     let result_string = match result {
         Ok(matching_line) => format!("{:?}", matching_line),
@@ -31,7 +31,8 @@ async fn queryMany(table: &str, data: &str) -> String {
     println!("Data: {}", data);
     let extension: &str = ".json";
     let together = format!("{}{}", table, extension);
-    let result = find::search_many(&together, data);
+    let query: &str = functions::query_format(data);
+    let result = find::search_many(&together, query);
     let result_string = match result {
         Ok(matching_lines) => format!("{:?}", matching_lines),
         Err(error) => format!(r#"{{"error":"{}"}}"#, error),
