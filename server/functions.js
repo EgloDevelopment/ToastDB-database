a57937d950ab87a57792914ed0c364e1b1510f07
EgloDevelopment <UNUstifA4W346TcF2Qwc@gmail.com>
2023-03-12 07:07:13 +0000

    Made it work lol

--- server/functions.js
+++ server/functions.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+const dJSON = require('dirty-json')
 var Datastore = require('nedb')
 
 var databases = new Datastore({filename: './database/databases.json', autoload: true})
-var tables = new Datastore({filename: './database/tables.json', autoload: true})
 var errors = new Datastore({filename: './database/errors.json', autoload: true})
 
 function getRandomInt(max) {
@@ -21,30 +21,27 @@ router.post('/', (req, res) => {
     res.json({"status":0})
 })
 
+
+
+
+
+
 // DB FIND
 router.post('/find/:table', (req, res) => {
     let table = req.params.table
-    tables.find({ name: table }, function(err, docs) {
-        if (docs[0] === undefined) {
-            res.json({"status":1,"error":"table does not exist"});
-            return
-        }
-    });
     databases.find({ enabled: true }, async function(err, docs) {
         try {
             const JsonAggreagate = []
             for (const val of docs) {
                 var url = val.url + "/find/" + table
                 let result = await axios.post(url, req.body)
-                for (const val of result.data) {
-                    JsonAggreagate.push(JSON.parse(val))
+                for (const val of result.data.result) {
+                    JsonAggreagate.push(dJSON.parse(val))
                 };
             }
             res.json({"status":0,"result":JsonAggreagate});
         } catch (error) {
-            res.json({"status":1,"error":error});
-            errors.insert({"error":error}, function (err, result) {});
-            console.log(error)
+            res.json({"status":0,"result":[]});
         }
     })
 })
@@ -52,22 +49,20 @@ router.post('/find/:table', (req, res) => {
 // DB INSERT
 router.post('/insert/:table', (req, res) => {
     let table = req.params.table
-    tables.find({ name: table }, function(err, docs) {
-        if (docs[0] === undefined) {
-            res.json({"status":1,"error":"table does not exist"});
-            return
-        }
-    });
     databases.find({ enabled: true }, async function(err, docs) {
         try {
-            let items = docs.length()
-            let node = getRandomInt(items + 1)
-            var url = val.url[node] + "/insert/" + table
+            let items = docs.length
+            let node = getRandomInt(items)
+            var url = docs[node].url + "/insert/" + table
             let result = await axios.post(url, req.body)
-            if (result.status === 0) {
-                res.json({"status":0});
-            } else {
-                res.json({"status":1,"error":result.error});
+            try {
+                if (result.status === 0) {
+                    res.json({"status":0});
+                } else {
+                    await axios.post("/insert/" + table, req.body)
+                }
+            } catch {
+                await axios.post("/insert/" + table, req.body)
             }
         } catch (error) {
             console.log(error)
@@ -79,12 +74,6 @@ router.post('/insert/:table', (req, res) => {
 // DB DELETE
 router.post('/delete/:table', (req, res) => {
     let table = req.params.table
-    tables.find({ name: table }, function(err, docs) {
-        if (docs[0] === undefined) {
-            res.json({"status":1,"error":"table does not exist"});
-            return
-        }
-    });
     databases.find({ enabled: true }, async function(err, docs) {
         try {
             for (const val of docs) {
@@ -100,14 +89,77 @@ router.post('/delete/:table', (req, res) => {
     })
 })
 
+
+
+
+
+
+// TABLE NEW
+router.post('/create-table/:table', (req, res) => {
+    let table = req.params.table
+    databases.find({ enabled: true }, async function(err, docs) {
+        try {
+            for (const val of docs) {
+                var url = val.url + "/create-table/" + table
+                await axios.post(url, req.body)
+            }
+            res.json({"status":0});
+        } catch (error) {
+            res.json({"status":1,"error":error});
+            errors.insert({"error":error}, function (err, result) {});
+            console.log(error)
+        }
+    })
+})
+
+// TABLE DELETE
+router.post('/delete-table/:table', (req, res) => {
+    let table = req.params.table
+    databases.find({ enabled: true }, async function(err, docs) {
+        try {
+            for (const val of docs) {
+                var url = val.url + "/delete-table/" + table
+                await axios.post(url, req.body)
+            }
+            res.json({"status":0});
+        } catch (error) {
+            res.json({"status":1,"error":error});
+            errors.insert({"error":error}, function (err, result) {});
+            console.log(error)
+        }
+    })
+})
+
+// TABLE LIST
+router.post('/list-tables', (req, res) => {
+    databases.find({ enabled: true }, async function(err, docs) {
+        try {
+            const JsonAggreagate = []
+            for (const val of docs) {
+                var url = val.url + "/list-tables"
+                let result = await axios.post(url, req.body)
+                for (const val of result.data.result) {
+                    if (!JsonAggreagate.includes(val)) {
+                        JsonAggreagate.push(dJSON.parse(val))
+                    }
+                };
+            }
+            res.json({"status":0,"result":JsonAggreagate});
+        } catch (error) {
+            res.json({"status":1,"error":error});
+            errors.insert({"error":error}, function (err, result) {});
+            console.log(error)
+        }
+    })
+})
+
+
+
+
+
+
 // CLUSTER NEW
 router.post('/new-cluster', (req, res) => {
-    tables.find({ name: req.body.name }, function(err, docs) {
-        if (docs[0] !== 'undefined') {
-            res.json({"status":1,"error":"cluster with same name exists"});
-            return;
-        }
-    });
     databases.insert({ name: req.body.name, enabled: req.body.enabled, url: req.body.url }, async function(err, docs) {
         try {
             var url = req.body.url + "/test"
